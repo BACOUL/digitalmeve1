efb0fa550614c33654db4d27bed91291df6e6a14
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-09-10 14:37:21 +0200

    Create Portal.tsx

---
+++ components/Portal.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+type Props = { children: React.ReactNode };
+
+export default function Portal({ children }: Props) {
+  const elRef = useRef<HTMLDivElement | null>(null);
+  if (!elRef.current) elRef.current = document.createElement("div");
+
+  useEffect(() => {
+    const el = elRef.current!;
+    document.body.appendChild(el);
+    return () => {
+      document.body.removeChild(el);
+    };
+  }, []);
+
+  return createPortal(children, elRef.current);
+}
