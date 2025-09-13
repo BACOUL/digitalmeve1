6a540219e137f01d23da9a1ef80042db8adcea88
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-09-13 18:07:22 +0200

    Update Portal.tsx

--- components/Portal.tsx
+++ components/Portal.tsx
@@ -5,10 +5,13 @@ import { createPortal } from "react-dom";
 
 export default function Portal({ children }: { children: React.ReactNode }) {
   const [mounted, setMounted] = useState(false);
+
   useEffect(() => {
     setMounted(true);
     return () => setMounted(false);
   }, []);
-  if (!mounted) return null;              // évite tout accès à document côté SSR
+
+  if (!mounted) return null;
+
   return createPortal(children, document.body);
 }
